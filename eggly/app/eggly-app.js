049558b77a85f08f6faea7baefcccb6740d3e317
blakar <blakar@users.noreply.github.com>
2016-10-04 11:01:40 -0700

    Manage UI states

--- eggly/app/eggly-app.js
+++ eggly/app/eggly-app.js
@@ -22,9 +22,14 @@
     ];
 
     $scope.currentCategory = null;
+    $scope.isCreating = false;
+    $scope.isEditing = false;
 
     function setCurrentCategory(category) {
         $scope.currentCategory = category;
+
+        cancelCreating();
+        cancelEditing();
     }
 
     function isCurrentCategory(category)
@@ -33,6 +38,38 @@
             category.name === $scope.currentCategory.name;
     }
 
+    function startCreating() {
+        $scope.isCreating = true;
+        $scope.isEditing = false;
+    }
+
+    function cancelCreating() {
+        $scope.isCreating = false;
+    }
+
+    function startEditing() {
+        $scope.isCreating = false;
+        $scope.isEditing = true;
+    }
+
+    function cancelEditing() {
+        $scope.isEditing = false;
+    }
+
+    function shouldShowCreating() {
+        return $scope.currentCategory && !$scope.isEditing;
+    }
+
+    function shouldShowEditing() {
+        return $scope.isEditing && !$scope.isCreating;
+    }
+
     $scope.setCurrentCategory = setCurrentCategory;
     $scope.isCurrentCategory = isCurrentCategory;
+    $scope.startCreating = startCreating;
+    $scope.cancelCreating = cancelCreating;
+    $scope.startEditing = startEditing;
+    $scope.cancelEditing = cancelEditing;
+    $scope.shouldShowCreating = shouldShowCreating;
+    $scope.shouldShowEditing = shouldShowEditing;
 });
