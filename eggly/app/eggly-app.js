48d33713118bd488eb86e61b9dbeef64e44e8ff5
blakar <blakar@users.noreply.github.com>
2016-10-04 00:09:19 -0700

    Highlight currently selected category.

--- eggly/app/eggly-app.js
+++ eggly/app/eggly-app.js
@@ -27,5 +27,12 @@
         $scope.currentCategory = category;
     }
 
+    function isCurrentCategory(category)
+    {
+        return $scope.currentCategory !== null &&
+            category.name === $scope.currentCategory.name;
+    }
+
     $scope.setCurrentCategory = setCurrentCategory;
+    $scope.isCurrentCategory = isCurrentCategory;
 });
